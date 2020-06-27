48d302e87a07d5d7aa63a1993d7094fcb9805ad8
kano-kenji <aleksandrtarasevich77@gmail.com>
2020-06-27 14:17:39 -0600

    Working variant add todo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import {connect} from "react-redux";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(props) {
+    const [title, setTitle] = useState('');
+
+    const addTodo = () => {
+        props.addTodo(title);
+        setTitle('');
+    }
+
+    return (
+        <div className="App">
+            React/Redux Todo list
+            <ul>
+                {
+                    props.todos.map((el, idx)=>
+                        <li key={idx}>
+                            {el.title}
+                        </li>
+                    )
+                }
+            </ul>
+            <input type="text" value={title} onChange={(e) => setTitle(e.target.value)}/>
+            <button onClick={addTodo}>Add</button>
+        </div>
+    );
 }
 
-export default App;
+const mapStateToProps = (state) => ({
+    todos: state.todos
+});
+
+const mapDispatchToProps = (dispatch) => ({
+    addTodo: (todo) => dispatch({ type: 'TODO_ADD', payload: todo })
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
---
+++ src/redux/reducer.js
@@ -0,0 +1,25 @@
+
+const initialState = {
+    todos: [
+        {
+            title: 'First todo',
+            done: true,
+        }, {
+            title: 'Second todo',
+            done: false,
+        }
+    ]
+};
+
+const todo = (state = initialState, action) => {
+    switch (action.type) {
+        case 'TODO_ADD':
+            return {...state,
+                todos: [...state.todos, {title: action.payload, done: false}]
+            }
+        default:
+            return state;
+    }
+}
+
+export default todo;
