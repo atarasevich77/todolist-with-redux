10786b5142e9807ef102c16bcc629da04174dd6e
kano-kenji <aleksandrtarasevich77@gmail.com>
2020-06-29 11:59:00 -0600

    Updates

---
+++ precache-manifest.908f49684e234207d9fae4137d1bb6f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b3de0a5a91a63e87140fe70f887d8b6",
+    "url": "/todolist-with-redux/index.html"
+  },
+  {
+    "revision": "970b9cc02e67470cdbac",
+    "url": "/todolist-with-redux/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0eb97000b7aee9583524",
+    "url": "/todolist-with-redux/static/js/2.9d653c43.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/todolist-with-redux/static/js/2.9d653c43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "970b9cc02e67470cdbac",
+    "url": "/todolist-with-redux/static/js/main.5a24a83b.chunk.js"
+  },
+  {
+    "revision": "d13c8abed09efd5ee227",
+    "url": "/todolist-with-redux/static/js/runtime-main.11fa273f.js"
+  }
+]);
