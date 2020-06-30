373a0f2f71757a1a6245a68135470396fc5809e6
kano-kenji <aleksandrtarasevich77@gmail.com>
2020-06-30 08:35:31 -0600

    Add styles for create form

--- src/components/CreateTodo/CreateTodo.js
+++ src/components/CreateTodo/CreateTodo.js
@@ -4,16 +4,31 @@ import {connect} from "react-redux";
 const CreateTodo = (props) => {
     const [title, setTitle] = useState('');
 
-    const addTodo = () => {
+    const addTodo = (e) => {
+        e.preventDefault();
         props.addTodo(title);
         setTitle('');
     }
 
     return (
-        <div>
-            <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} />
-            <button onClick={addTodo}>Add</button>
-        </div>
+        <form className="form-inline">
+            <div className="form-group mb-2">
+                <label htmlFor="name" className="sr-only">Name</label>
+                <input type="text" className="form-control" placeholder="Name"
+                       value={title}
+                       onChange={(e) => setTitle(e.target.value)}
+
+                />
+            </div>
+            <div className="form-group mx-sm-3 mb-2">
+                <label htmlFor="desc" className="sr-only">Description</label>
+                <input type="password" className="form-control" placeholder="Description"
+                       value={title}
+                       onChange={(e) => setTitle(e.target.value)}
+                />
+            </div>
+            <button className="btn btn-success mb-2" onClick={addTodo}>Add</button>
+        </form>
     );
 };
 
